dab63cffe39e0bfdec11509738811714b2f3efc5
Waleed Gamal <waleed.gamal1000@gmail.com>
2018-10-15 23:22:10 +0200

    add normalize.css

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
-import './main.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
+import 'normalize.css/normalize.css';
+import './main.css';
 
 render(
   <Provider store={store}>
