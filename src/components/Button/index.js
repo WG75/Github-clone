82c96c1321173bc6cc03d1a75ee51eae22459ba7
Waleed Gamal <waleed.gamal1000@gmail.com>
2018-10-16 03:13:18 +0200

    added Button component

---
+++ src/components/Button/index.js
@@ -0,0 +1,58 @@
+// @flow
+
+import React, { type Node } from 'react';
+import { Link } from 'react-router-dom';
+import './Button.css';
+
+type props = {
+  className?: string,
+  onClick?: () => void,
+  isLink?: boolean,
+  path?: string,
+  children: Node,
+  disabled?: boolean,
+  type?: string,
+  isPrimary?: boolean,
+  isSecondary?: boolean,
+};
+
+const Button = ({
+  className,
+  onClick,
+  isLink,
+  path,
+  children,
+  disabled,
+  type,
+  isPrimary,
+  isSecondary,
+}: props) => (isLink ? (
+  <Link
+    className={`button ${isPrimary ? 'button--is_primary' : ''} ${
+      isSecondary ? 'button--is_secondary' : ''
+    }  isLink ${className || ''}`}
+    to={path || '/'}
+  >
+    {children}
+  </Link>
+) : (
+  <button
+    disabled={disabled}
+    className={`button ${isPrimary ? 'button--is_primary' : ''} ${
+      isSecondary ? 'button--is_secondary' : ''
+    } ${className || ''}`}
+    onClick={onClick}
+    type={type}
+  >
+    {children}
+  </button>
+));
+
+Button.defaultProps = {
+  isLink: false,
+  disabled: false,
+  isPrimary: true,
+  type: 'button',
+};
+
+export default Button;
