3f2da41359dc4a970aec9f743683e8f313a4e41b
Waleed Gamal <waleed.gamal1000@gmail.com>
2018-10-17 06:49:46 +0200

    fixed a bug

--- src/config/apiService.js
+++ src/config/apiService.js
@@ -1,3 +1,3 @@
-export default (node.process.NODE_ENV === 'production'
-  ? 'http://localhost:3030'
-  : 'http://localhost:3000');
+export default (process.NODE_ENV === 'production'
+  ? 'http://localhost:3030/api'
+  : 'http://localhost:3000/api');
