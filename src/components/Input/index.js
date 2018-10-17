50a6e558eb6f3073ad67ca6013156ea64de2bdbb
Waleed Gamal <waleed.gamal1000@gmail.com>
2018-10-17 05:08:04 +0200

    change input component to functional component

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -9,53 +9,42 @@ type Props = {
   label: string,
   type?: string,
   className?: string,
-};
-
-type State = {
   value: string,
 };
 
-export default class TextInput extends React.Component<Props, State> {
-  static defaultProps = {
-    type: 'text',
+type State = {};
+
+const TextInput = ({
+  id, label, type, className, value, onChange,
+}: Props) => {
+  const handleChange = (e: any): void => {
+    const value = e.target.value;
+
+    if (onChange) {
+      onChange(id, value);
+    }
   };
 
-  state = {
-    value: '',
-  };
-
-  handleChange(e: any) {
-    this.setState({ value: e.target.value }, () => {
-      const { onChange, id } = this.props;
-      const { value } = this.state;
-
-      if (onChange) {
-        onChange(id, value);
-      }
-    });
-  }
-
-  render() {
-    const {
-      id, label, type, className,
-    }: Props = this.props;
-
-    const { value } = this.state;
-    return (
-      <label className={`${'input_wapper'} ${className || ''}`} htmlFor={id}>
-        <span className="input_wapper__label">
-          {label}
+  return (
+    <label className={`${'input_wapper'} ${className || ''}`} htmlFor={id}>
+      <span className="input_wapper__label">
+        {label}
 :
-        </span>
-        <input
-          className="input_wapper__input"
-          type={type}
-          id={id}
-          name={id}
-          value={value}
-          onChange={(e: any) => this.handleChange(e)}
-        />
-      </label>
-    );
-  }
-}
+      </span>
+      <input
+        className="input_wapper__input"
+        type={type}
+        id={id}
+        name={id}
+        value={value}
+        onChange={handleChange}
+      />
+    </label>
+  );
+};
+
+TextInput.defaultProps = {
+  type: 'text',
+};
+
+export default TextInput;
