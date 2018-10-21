99f838a500a62d77a1ac5afc11018d75bc2313db
Waleed Gamal <waleed.gamal1000@gmail.com>
2018-10-21 09:34:36 +0200

    remove mongoose

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const path = require('path');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
@@ -21,19 +20,6 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.join(__dirname, '../build', 'index.html'));
   });
 }
-
-const mongoDB = 'mongodb://127.0.0.1/maersk';
-
-mongoose.connect(
-  mongoDB,
-  { useNewUrlParser: true },
-);
-
-mongoose.Promise = global.Promise;
-
-const db = mongoose.connection;
-
-db.on('error', err => console.error('MongoDB connection error:', err));
 
 app.listen(3030, () => {
   console.log('app is up and ruuning on port 3030');
--- server/models/index.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const { Schema } = mongoose;
-
-const Model = new Schema({});
-
-module.exports = mongoose.model('model', Model);
